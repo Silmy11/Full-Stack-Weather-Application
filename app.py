c42d97ce5724f4f48b229a419f5757908650c5ca
Silmy11 <sd.silmy@gmail.com>
2026-06-22 22:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,12 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY', 'super_secret_weather_key')
 # MySQL Configuration matching your target schemas
 # Force-connect directly to your cloud DB strings
 # MySQL Configuration matching your target schemas
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
-app.config['MYSQL_DB'] = 'weather_db'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_HOST'] = os.getenv('DB_HOST')
+app.config['MYSQL_USER'] = os.getenv('DB_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('DB_NAME')
+app.config['MYSQL_PORT'] = int(os.getenv('DB_PORT'))
+
 
 mysql = MySQL(app)
 
